a72fd441060c517461d2877fa90cf097f1a50625
yiding he <yidinghe2012@gmail.com>
2017-12-27 14:39:55 -0500

    Added BirdScript.

---
+++ Assets/Scripts/Bird Script/BirdScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+	public static BirdScript instance;
+
+	[SerializeField]
+	private Rigidbody2D myRigidBody;
+
+	[SerializeField]
+	private Animator anim;
+
+	private float forwardSpeed = 3f;
+
+	private float bounceSpeed = 4f;
+
+	private bool didFlap;
+
+	public bool isAlive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
